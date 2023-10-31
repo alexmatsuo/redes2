b8291a5c8f839d9acbfebdf9288e4bc27925f236
alexmatsuo <104271444+alexmatsuo@users.noreply.github.com>
2023-10-31 17:42:19 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 import socket
 import time
 import sys
+import pickle  # For serialization
 
 localIP = "127.0.0.1"
-localPort = 20001
+localPort = 8888
 bufferSize = 1024
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -11,20 +12,26 @@ UDPServerSocket.bind((localIP, localPort))
 
 print("UDP server up and streaming")
 
-sequence = 0
+class Packet:
+    def __init__(self, index, data):
+        self.index = index
+        self.data = data
+
+packet = Packet(index=0, data=1)
 delay = 1  # Default delay
 
 if len(sys.argv) > 1:
     delay = float(sys.argv[1])
 
 while True:
-    sequence += 1
-    message = f"Sequence: {sequence}"
-    bytesToSend = str.encode(message)
+    # Serialize the packet object to bytes using pickle
+    serialized_packet = pickle.dumps(packet)
 
     # Broadcast message to all clients
-    UDPServerSocket.sendto(bytesToSend, (localIP, localPort))
+    UDPServerSocket.sendto(serialized_packet, (localIP, localPort))
 
-    print(f"Streamed: {message}")
+    print(f"Index - {packet.index}, Data - {packet.data}")
+    packet.index += 1
+    packet.data += 1
 
     time.sleep(delay)
