d1860d88a9a70ecf0c2622b888c7e2165df9c032
alexmatsuo <104271444+alexmatsuo@users.noreply.github.com>
2023-11-01 18:52:26 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,5 +1,17 @@
 import socket
 import pickle
+import logging
+
+# Configure logging for the client
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a file handler for writing logs to a file
+file_handler = logging.FileHandler('client_logs.log')
+file_handler.setLevel(logging.INFO)
+
+# Add the file handler to the logger
+logger = logging.getLogger()
+logger.addHandler(file_handler)
 
 class Packet:
     def __init__(self, index, data):
@@ -12,7 +24,7 @@ serverAddressPort = ("127.0.0.1", 8889)
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPClientSocket.bind(("127.0.0.1", 8889))
 
-print("UDP client listening")
+logging.info("UDP client listening")
 
 total_data = 0
 
@@ -20,7 +32,7 @@ try:
     while True:
         serialized_packet, _ = UDPClientSocket.recvfrom(bufferSize)
         packet = pickle.loads(serialized_packet)
-        print(f"Received: Index - {packet.index}, Data - {packet.data}")
+        logging.info(f"Received: Index - {packet.index}, Data - {packet.data}")
         total_data += packet.data
 except KeyboardInterrupt:
-    print(f"\nTotal sum of received data: {total_data}\n")
+    logging.info(f"Total sum of received data: {total_data}")
--- server.py
+++ server.py
@@ -1,6 +1,18 @@
 import socket
 import pickle
 import time
+import logging
+
+# Configure logging for the server
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Create a file handler for writing logs to a file
+file_handler = logging.FileHandler('server_logs.log')
+file_handler.setLevel(logging.INFO)
+
+# Add the file handler to the logger
+logger = logging.getLogger()
+logger.addHandler(file_handler)
 
 localIP = "127.0.0.1"
 localPort = 8888
@@ -9,7 +21,7 @@ bufferSize = 1024
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 UDPServerSocket.bind((localIP, localPort))
 
-print("UDP server up and streaming")
+logging.info("UDP server up and streaming")
 
 class Packet:
     def __init__(self, index, data):
@@ -22,11 +34,11 @@ delay = 1
 while True:
     try:
         serialized_packet = pickle.dumps(packet)
-        UDPServerSocket.sendto(serialized_packet, ("127.0.0.1", 8889))  # Send to client's address and port
-        print(f"Index - {packet.index}, Data - {packet.data}")
+        UDPServerSocket.sendto(serialized_packet, ("127.0.0.1", 8889))
+        logging.info(f"Index - {packet.index}, Data - {packet.data}")
         packet.index += 1
         packet.data += 1
         time.sleep(delay)
     except KeyboardInterrupt:
-        print("\n")
+        logging.info("Server stopped.")
         break
