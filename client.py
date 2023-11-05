4a3ebae46c953f2b2701d7ccf8af8152bb588c3b
alexmatsuo <104271444+alexmatsuo@users.noreply.github.com>
2023-11-05 17:18:02 -0300

    Add files via upload

--- client.py
+++ client.py
@@ -41,6 +41,10 @@ while not binded:
 logging.info("UDP client listening on port: " + str(port) + "\n")
 
 total_data = 0
+total_packets_received = 0
+previous_index = -1
+packets_lost = 0
+packets_out_of_order = 0
 
 # Envia mensagem para o servidor para conectar a ele
 bytesToSend = str.encode("Connect message")
@@ -51,8 +55,23 @@ try:
     while True:
         serialized_packet, _ = UDPClientSocket.recvfrom(bufferSize)
         packet = pickle.loads(serialized_packet)
+        total_packets_received += 1
+
+        # Check for packet order and count lost/out of order packets
+        if packet.index == previous_index + 1:
+            total_data += packet.data
+        else:
+            if packet.index > previous_index + 1:
+                packets_lost += packet.index - (previous_index + 1)
+            else:
+                packets_out_of_order += 1
+
         logging.info(f"Received: Index - {packet.index}, Data - {packet.data}")
-        total_data += packet.data
+        previous_index = packet.index
+
 except KeyboardInterrupt:
-    # Imprime a soma total de dados recebidos
+    # Imprime a soma total de dados recebidos e estatísticas de pacotes
     logging.info(f"Total sum of received data: {total_data}")
+    logging.info(f"Total packets received: {total_packets_received}")
+    logging.info(f"Total packets lost: {packets_lost}")
+    logging.info(f"Packets out of order: {packets_out_of_order}")
